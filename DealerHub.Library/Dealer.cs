2cfdfef5e59ab829449af16d73cfb288ef7ad4e7
Peter Jones <peter.jones@loyaltylogistix.com>
2024-07-22 12:00:23 +0100

    add dealer address

--- DealerHub.Library/Dealer.cs
+++ DealerHub.Library/Dealer.cs
@@ -2,19 +2,24 @@
 public class Dealer
 {
     public string Name { get; private set; } = string.Empty;
+    public string Address { get; private set; } = string.Empty;
     public string Telephone { get; private set; } = string.Empty;
 
-    public Dealer(string name, string telephone)
+    public Dealer(string name, string address, string telephone)
     {
         UpdateName(name);
+        UpdateAddress(address);
         UpdateTelephone(telephone);
     }
-
-
     private void UpdateName(string newName)
     {
         ArgumentNullException.ThrowIfNull(newName);
         Name = newName;
+    }
+    private void UpdateAddress(string newAddress)
+    {
+        ArgumentNullException.ThrowIfNull(newAddress);
+        Address = newAddress;
     }
 
     private void UpdateTelephone(string newTelephone)
