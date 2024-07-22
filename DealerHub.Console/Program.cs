f4f2c80b7e4234078222befbb20622c46b9273db
BPJones3D <bpjones3d@gmail.com>
2024-07-22 13:15:27 +0100

    fixed missing properties for customer, added for loop with cleaner writeline infrastructure

--- DealerHub.Console/Program.cs
+++ DealerHub.Console/Program.cs
@@ -3,15 +3,27 @@
 var dealer1 = new Dealer("ABC Motors", "Llandudno", "01492 123456");
 var dealer2 = new Dealer("XYZ Motors", "Deganwy", "01492 654321");
 
-var customer1 = new Customer("Alan", "Davis", "Penrhyn Bay", "01492 827388");
-var customer2 = new Customer("Jessica", "Thomas", "Conwy", "01492 193823");
+
+List<Customer> customer = new List<Customer>();
+var customer1 = new Customer("Alan", "Davis", "52", "Penrhyn Bay", "Conwy", "United Kingdom","LL31 8TA", "87938 173827", "01492 827388", "alandavis@gmail.com");
+var customer2 = new Customer("Jessica", "Thomas", "46", "Conwy", "Conwy", "United Kingdom", "LL31 6YA", "837263 827462", "01492 193823", "jessthombis@hotmail.co.uk");
+customer.Add(customer1);
+customer.Add(customer2);
 
 Console.WriteLine($"Dealer 1: {dealer1.Name} - {dealer1.Address} - {dealer1.Telephone}");
 Console.WriteLine($"Dealer 2: {dealer2.Name} - {dealer2.Address} - {dealer2.Telephone}");
-Console.WriteLine("\r\n");
+Console.WriteLine();
 
-Console.WriteLine($"Customer 1: {customer1.FirstName} {customer1.LastName} - {customer1.Address} - {customer1.Telephone}");
-Console.WriteLine($"Customer 2: {customer2.FirstName} {customer2.LastName} - {customer2.Address} - {customer2.Telephone}");
+for( int i = 0; i < customer.Count; i++ )
+{
+    var elem = customer[i]; 
 
+    Console.WriteLine($"Customer {i+1}: {elem.FirstName} {elem.LastName} ({elem.Age})");
+    Console.WriteLine($"{elem.AddressLine1}, {elem.AddressLine2}, {elem.AddressLine3}, {elem.PostCode}");
+    Console.WriteLine($"Home Tel: {elem.HomeTelNo}");
+    Console.WriteLine($"Mobile Tel: {elem.MobileTelNo}");
+    Console.WriteLine($"Email: {elem.EmailAddress}");
+    Console.WriteLine();
+}
 
 Console.ReadLine();
--- DealerHub.Domain/Dealer.cs
+++ DealerHub.Domain/Dealer.cs
@@ -32,14 +32,22 @@ public class Customer
 {
     public string FirstName { get; private set; } = string.Empty;
     public string LastName { get; private set; } = string.Empty;
-    public string Address { get; private set; } = string.Empty;
-    public string Telephone { get; private set; } = string.Empty;
+    public string Age { get; private set; } = string.Empty;
+    public string AddressLine1 { get; private set; } = string.Empty;
+    public string AddressLine2 { get; private set; } = string.Empty;
+    public string AddressLine3 { get; private set; } = string.Empty;
+    public string PostCode { get; private set; } = string.Empty;
+    public string HomeTelNo { get; private set; } = string.Empty;
+    public string MobileTelNo { get; private set; } = string.Empty;
+    public string EmailAddress { get; private set; } = string.Empty;
 
-    public Customer(string firstname, string lastname, string address, string telephone)
+
+    public Customer(string firstname, string lastname, string age, string addressline1, string addressline2, string addressline3, string postcode, string hometelno, string mobiletelno, string emailaddress)
     {
         UpdateName(firstname, lastname);
-        UpdateAddress(address);
-        UpdateTelephone(telephone);
+        UpdateAge(age);
+        UpdateAddress(addressline1, addressline2, addressline3);
+        UpdateContact(hometelno, mobiletelno, emailaddress);
     }
 
     private void UpdateName(String newFirstName, String newLastName)
@@ -49,14 +57,27 @@ public class Customer
         FirstName = newFirstName;
         LastName = newLastName;
     }
-    private void UpdateAddress(String newAddress)
+    private void UpdateAge(String newAge)
     {
-        ArgumentNullException.ThrowIfNull(newAddress);
-        Address = newAddress;
+        ArgumentNullException.ThrowIfNull(newAge);
+        Age = newAge;
     }
-    private void UpdateTelephone(String newTelephone)
+    private void UpdateAddress(String newAddressLine1, String newAddressLine2, String newAddressLine3)
     {
-        ArgumentNullException.ThrowIfNull(newTelephone);
-        Telephone = newTelephone;
+        ArgumentNullException.ThrowIfNull(newAddressLine1);
+        ArgumentNullException.ThrowIfNull(newAddressLine2);
+        ArgumentNullException.ThrowIfNull(newAddressLine3);
+        AddressLine1 = newAddressLine1;
+        AddressLine2 = newAddressLine2;
+        AddressLine3 = newAddressLine3;
+    }
+    private void UpdateContact(String newHomeTelNo, String newMobileTelNo, String newEmailAddress)
+    {
+        ArgumentNullException.ThrowIfNull(newHomeTelNo);
+        ArgumentNullException.ThrowIfNull(newMobileTelNo);
+        ArgumentNullException.ThrowIfNull(newEmailAddress);
+        HomeTelNo = newHomeTelNo;
+        MobileTelNo = newMobileTelNo;
+        EmailAddress = newEmailAddress;
     }
 }
