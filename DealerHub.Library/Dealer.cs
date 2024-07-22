556233d867cfff395fb99340a4231c2b359a9d0f
Peter Jones <peter.jones@loyaltylogistix.com>
2024-07-22 11:55:41 +0100

    setup initial solution

---
+++ DealerHub.Library/Dealer.cs
@@ -0,0 +1,25 @@
+﻿namespace DealerHub.Library;
+public class Dealer
+{
+    public string Name { get; private set; } = string.Empty;
+    public string Telephone { get; private set; } = string.Empty;
+
+    public Dealer(string name, string telephone)
+    {
+        UpdateName(name);
+        UpdateTelephone(telephone);
+    }
+
+
+    private void UpdateName(string newName)
+    {
+        ArgumentNullException.ThrowIfNull(newName);
+        Name = newName;
+    }
+
+    private void UpdateTelephone(string newTelephone)
+    {
+        ArgumentNullException.ThrowIfNull(newTelephone);
+        Telephone = newTelephone;
+    }
+}
